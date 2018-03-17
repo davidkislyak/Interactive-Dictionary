230fded51253f03f8e6cfa159b5af69506a95ccb
David_Ruban <davidruban9@gmail.com>
2018-03-16 22:21:52 -0700

    Implement Pull Request #1, remove text warping, update logic to handle more user fuzzing.

--- dictionary_app.py
+++ dictionary_app.py
@@ -3,53 +3,55 @@
 # 3/15/2018
 # CLI dictionary application
 
+from difflib import get_close_matches
 from json import load
-from difflib import SequenceMatcher, get_close_matches
 
 data = load(open("data.json"))  # secretly is: open("data.json", "r")
 
 def word_lookup(word):
     #__init__
-    close_matches = []
+    #close_matches = []
     word = word.lower()
 
     # Attempt to look for the user input in the dictionary 
     try:
         definition_list = data[word]
 
-    # If that fails and receives a KeyError:
+    # If that fails and raises a KeyError:
     except KeyError:
 
         # Attempt to find similar words to the users word
         close_matches = get_close_matches(user_input, data.keys())
 
-        # If there are no close_matches:
-        if len(close_matches) == 0:
-            return["I can't find that word. Please double check the word."]
-
-        # If there is at least one close match:
-        else:
+        # If there any close matches:
+        if len(close_matches) > 0:
             print("\nDid you mean: ")
 
             # For every close match found:
-            for i in range(len(close_matches)):
-                print(str(i + 1) + ".) " + str(close_matches[i]).capitalize())
+            num = 1
+            for word in close_matches:
+                print(f'{num}.) {word.capitalize()}')
+                num += 1
             print("0.) My word is not on here")
 
             # Gather user input about word selection and subtract one to index properly
             user_correct_word = int(input("\nWhat is your word: ")) - 1
 
-            # If user selected that the word is not on the list:
-            if user_correct_word == - 1:
-                return["Sorry I guess I can't find that word."]
+            # If user selected an option that is not available
+            if user_correct_word <= -2 or user_correct_word > len(close_matches) - 1:
+                raise ValueError("Sorry, that doesn't seem like an option.")
+
+            # If user selects that their word is not on the list:
+            elif user_correct_word == -1:
+                raise ValueError("Sorry I guess I can't find that word.")
 
             # If the user has picked a word from the list:
             elif user_correct_word <= len(close_matches) - 1:
                 return data[close_matches[user_correct_word]]
 
-            # If the user has entered a non existent index:
-            else:
-                return["Sorry that option does not exist."]
+        else:
+            # There are no close_matches:
+            raise ValueError("I can't find that word. Please double check the word.")
             
     return definition_list
 
@@ -58,50 +60,14 @@ def word_lookup(word):
 user_input = input("What word would you like to look up the definition for: ")
 
 # Call function word_lookup and save response to variable: definition
-definition = word_lookup(user_input)
+try:
+    definitions = word_lookup(user_input)
 
-# For each different definition returned:
-for i in definition:
-
-    # If the response is less than 104 characters:
-    if len(i) <= 103:
-        print("\n" + str(i))
-
-    # Else if character count > 104
-    else:
-
-        # Create Temporary variables 
-        temp_list = []
-        character_count = 0
-        final_string = ""
-
-        # Split the current sentence into a list of words (using spaces)
-        temp_list = i.split(" ")
-
-        # For each index (word) in temp_list:
-        for x in temp_list:
-
-            # Set character_count to the length of the word
-            character_count += len(x)
-
-            # If the amount of characters on the current line is less than 103:
-            if character_count <= 103:
-                final_string += " " + str(x)
-
-            # If the amount of characters printed on the current is more than 103:
-            elif character_count >= 103:
-                #               Start new line
-                final_string += "\n" + str(x)
-                # Reset character counter because a new line has started
-                character_count = 0
-
-            else:
-                character_count += len(x)
-                final_string += " " + str(x)
-
-        # Print out the end result    Remove whitespace
-        print("\n" + str(final_string).strip())
-
+    # for each different definition returned:
+    for definition in definitions:
+        print(f'{definition}\n')
+except ValueError as error:
+    print(error)
 
 # Keep program from automatically closing
 input()
